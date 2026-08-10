7903527aa97026d5d3989ae983c1f3d0ddcb0a43
Samantha Randolph <samcollegeemail@gmail.com>
2026-08-10 13:08:05 -0700

    Added comments to config.

--- config/system.c
+++ config/system.c
@@ -5,9 +5,11 @@
 #define CONFIG_WINDOWS 1
 #endif
 
+// From https://www.reddit.com/r/cprogramming/comments/oo655w/printing_unicode_characters_in_c/
 void configureTextEncoding() {
     if (CONFIG_WINDOWS) {
         SetConsoleOutputCP(CP_UTF8);
     }
     // Otherwise do nothing, UTF-8 should be default on Mac/UNIX.
 }
+
