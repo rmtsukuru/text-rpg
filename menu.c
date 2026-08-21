dfe9e95f6fe4f75be3c094f3d90e3e24e628be6a
Samantha Randolph <samcollegeemail@gmail.com>
2026-08-20 19:03:55 -0700

    Added proper validation for list menu prompts to keep selected index in bounds.

--- menu.c
+++ menu.c
@@ -5,13 +5,15 @@
 // Returns the index of the option selected by the user
 // (not the actual number typed into user input).
 byte listMenuPrompt(char* prompt, char** choices, byte length) {
-    byte result;
-    printf("\n%s\n", prompt);
-    for (int i = 0; i < length; i++) {
-        printf("%s - %d\n", choices[i], i + 1);
+    byte result = 0;
+    while (result <= 0 || result > length) {
+        printf("\n%s\n", prompt);
+        for (int i = 0; i < length; i++) {
+            printf("%s - %d\n", choices[i], i + 1);
+        }
+        printf("Make your choice (enter corresponding number): ");
+        scanf("%d", &result);
     }
-    printf("Make your choice (enter corresponding number): ");
-    scanf("%d", &result);
     return result - 1;
 }
 
